1e24d7af4d89784f60862324c902eb528000d65e
cyeongy <cyeongy@gmail.com>
2022-01-01 00:04:15 +0900

    Add Test Code
    HelloController, HelloResponseDto 메인, 테스트 코드 작성

--- src/main/java/com/example/springboot_basic/web/HelloController.java
+++ src/main/java/com/example/springboot_basic/web/HelloController.java
@@ -1,17 +1,22 @@
 package com.example.springboot_basic.web;
 
+import com.example.springboot_basic.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-//    @RequestMapping(method = RequestMethod.GET, value = "/")
-    @GetMapping("/")
-    public String Hello(){
+    //    @RequestMapping(method = RequestMethod.GET, value = "/")
+    @GetMapping("/hello")
+    public String hello() {
         return "Hello World!";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 
 }
--- src/test/java/com/example/springboot_basic/web/HelloControllerTest.java
+++ src/test/java/com/example/springboot_basic/web/HelloControllerTest.java
@@ -8,10 +8,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.is;
+import static org.assertj.core.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -23,7 +23,22 @@ class HelloControllerTest {
     public void helloTest() throws Exception {
         String hello = "Hello World!";
 
-        mvc.perform(get("/")).andExpect(status().isOk()).andExpect(content().string(hello));
+        mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
+    }
+
+    @Test
+    void helloDtoTest() throws Exception {
+        String name = "Mike";
+        int amount = 30;
+        mvc.perform(get("/hello/dto")
+                .param("name", name)
+                .param("amount", String.valueOf(amount))
+        )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount))
+                );
+
     }
 
 }
