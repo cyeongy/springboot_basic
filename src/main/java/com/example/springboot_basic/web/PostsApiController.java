aad19cfa8ffd39baa44d9b859897c2558a97da75
cyeongy <cyeongy@gmail.com>
2022-01-06 22:58:35 +0900

    Fix main code-PostsApiController
    GetMapping -> PostMapping 수정

--- src/main/java/com/example/springboot_basic/web/PostsApiController.java
+++ src/main/java/com/example/springboot_basic/web/PostsApiController.java
@@ -4,6 +4,7 @@ import com.example.springboot_basic.service.posts.PostsService;
 import com.example.springboot_basic.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostsApiController {
     private final PostsService postsService;
 
-    @GetMapping("/api/v1/posts")
+    @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
     }
