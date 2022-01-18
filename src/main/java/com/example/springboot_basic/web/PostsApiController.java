aed4d0a5b1f879916f45a36fb501f8da82225c2b
cyeongy <cyeongy@gmail.com>
2022-01-18 16:09:02 +0900

    Add logging code
    logback을 이용해서 requestBody 객체 확인

--- src/main/java/com/example/springboot_basic/web/PostsApiController.java
+++ src/main/java/com/example/springboot_basic/web/PostsApiController.java
@@ -5,25 +5,29 @@ import com.example.springboot_basic.web.dto.PostsResponseDto;
 import com.example.springboot_basic.web.dto.PostsSaveRequestDto;
 import com.example.springboot_basic.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class PostsApiController {
     private final PostsService postsService;
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @PostMapping("/api/v1/posts")
-    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
     }
 
     @PutMapping("/api/v1/posts/{id}")
-    public Long Update(@PathVariable("id") Long id, @RequestBody PostsUpdateRequestDto requestDto){
+    public Long Update(@PathVariable("id") Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        log.info("PostsApiController로그 : " + requestDto.getTitle() + requestDto.getContent());
         return postsService.update(id, requestDto);
     }
 
     @GetMapping("/api/v1/posts/{id}")
-    public PostsResponseDto findById(@PathVariable("id") Long id){
+    public PostsResponseDto findById(@PathVariable("id") Long id) {
         return postsService.findById(id);
     }
 }
